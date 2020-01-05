be7daeaa6642a81be294a33eeb1b98a3114b3102
Addanpadda <adamherbertsson@gmail.com>
2020-01-05 23:41:42 +0100

    Updated Storage

--- app/src/main/java/com/example/kotlin/Storage.kt
+++ app/src/main/java/com/example/kotlin/Storage.kt
@@ -1,29 +1,48 @@
 package com.example.kotlin
 
 import android.content.Context;
+import java.io.*
 
 
 interface Storage_Interface {
     var storagePath: String
 
     fun saveFile(fileName: String, content: String)
-    fun loadFile(fileName: String) : String
-    fun listFiles() : Array<String>
+    fun loadFile(fileName: String): String
+    fun listFiles(context: Context): Array<String>
 }
 
 
 class Storage: Storage_Interface {
-    override var storagePath = "files"
+    override var storagePath = String()
+
+    constructor(context: Context) {
+        // TODO: Seperate files dir for the files
+        // storagePath = context.filesDir+"/files"
+
+        storagePath = context.filesDir as String
+    }
 
     override fun saveFile(fileName: String, content: String) {
+        var file = File(storagePath, fileName)
+        if (!file.exists()) {
+            file.createNewFile()
+        }
 
+        var writer = BufferedWriter(FileWriter(file, false))
+        writer.write(content)
+        writer.close()
     }
 
     override fun loadFile(fileName: String): String {
+        var reader = BufferedReader(FileReader(fileName))
+        var content = reader.readText()
+        reader.close()
 
+        return content
     }
 
-    fun listFiles(context: Context) : Array<String> {
+    override fun listFiles(context: Context): Array<String> {
         return context.fileList()
     }
 }
